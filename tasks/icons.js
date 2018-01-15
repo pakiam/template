ddbe6312233261095491bbf1fc431d5ad27edf29
Maksim Loginov <akiam@mail.ru>
2018-01-16 00:57:10 +0300

    Initial commit

---
+++ tasks/icons.js
@@ -0,0 +1,33 @@
+import gulp from 'gulp';
+import svgSprite from 'gulp-svg-sprite';
+import gulpIf from 'gulp-if';
+import rename from 'gulp-rename';
+import plumber from 'gulp-plumber';
+import errorHandler from 'gulp-plumber-error-handler';
+
+gulp.task('icons', () => (
+	gulp.src('app/icons/**/*.svg')
+		.pipe(plumber({errorHandler: errorHandler(`Error in 'icons' task`)}))
+		.pipe(svgSprite({
+			mode: {
+				view: {
+					dest: './',
+					layout: 'diagonal',
+					sprite: 'default-svg',
+					bust: false,
+					render: {
+						scss: {
+							template: 'app/icons/sprite-template.scss'
+						}
+					}
+				},
+				symbol: true,		// Activate the «symbol» mode
+				variables: {
+					mapname: 'icons'
+				}
+			}
+		}))
+		.pipe(gulpIf(/\.scss$/, gulp.dest('app/styles/helpers')))
+		.pipe(gulpIf(/\.svg$/, rename('icon.svg')))
+		.pipe(gulpIf(/\.svg$/, gulp.dest('dist/assets/images/')))
+));
---
+++ tasks/styles.js
@@ -0,0 +1,31 @@
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import gulpIf from 'gulp-if';
+import sass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
+import gcmq from 'gulp-group-css-media-queries';
+import nano from 'gulp-cssnano';
+import rename from 'gulp-rename';
+import sourcemaps from 'gulp-sourcemaps';
+import errorHandler from 'gulp-plumber-error-handler';
+
+const isDebug = process.env.NODE_ENV !== 'production';
+
+gulp.task('styles', () => (
+	gulp.src('app/styles/*.scss')
+		.pipe(plumber({errorHandler: errorHandler(`Error in \'styles\' task`)}))
+		.pipe(gulpIf(isDebug, sourcemaps.init()))
+		.pipe(sass({
+			use: [
+				autoprefixer()
+			],
+			includePaths: ['node_modules/'],
+			outputStyle: 'compressed',
+			'include css': true
+		}))
+		.pipe(gulpIf(!isDebug, gcmq()))
+		.pipe(gulpIf(!isDebug, nano({zindex: false})))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulpIf(isDebug, sourcemaps.write()))
+		.pipe(gulp.dest('dist/assets/styles'))
+));
